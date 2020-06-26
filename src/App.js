a827102f6278f3c050784212fee97796c746f2c4
Norberto Ignacio <norbertoignaciojr@gmail.com>
2020-06-26 15:13:57 +0800

    finished

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from "react";
+import Post from "./components/post/Post";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [post,setPost] = useState( {
+      user: "Ken Kaneki",
+      username: "@OneEyedKing",
+      userLink: "something",
+      userPhoto: `https://i.ebayimg.com/images/g/t78AAOSw2sFdGt-s/s-l640.jpg`,
+      isThereText: true,
+      text: `I am the one eyed king. 👁`,
+      isThereImg: true,
+      imgSrc: `https://jikman.files.wordpress.com/2014/09/kaneki-ken-transformation.jpg`,
+      comments: ["Comment 1", "Comment 2"],
+      likeCount: 12,
+      shareCount: 2,
+  })
+        return (
+            <>
+                <Post post={post} />
+            </>
+        );
+
 }
 
 export default App;
